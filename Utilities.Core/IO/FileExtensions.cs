7546c9571ddce4fad7db2f1c7a3f6b2d7d7b658d
DamianMullins <damian@lowflyingowls.co.uk>
2013-05-07 17:50:19 +0100

    Added IO FileExtensions methods & tests

---
+++ Utilities.Core/IO/FileExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace System.Utilities.Core.IO
+{
+    public static class FileExtensions
+    {
+        public static string GetExtension(this string input)
+        {
+            var extension = Path.GetExtension(input);
+            return !string.IsNullOrEmpty(extension) ? extension.Remove(0, 1).ToLower() : string.Empty;
+        }
+    }
+}
---
+++ Utilities.Tests/Utilities.Core/IO/FileExtensionsTests.cs
@@ -0,0 +1,61 @@
+﻿using System.Utilities.Core.IO;
+using NUnit.Framework;
+
+namespace System.Utilities.Tests.Utilities.Core.IO
+{
+    [TestFixture]
+    public class FileExtensionsTests
+    {
+        [Test]
+        public void EmptyString_GetExtension_ReturnsEmptyString()
+        {
+            // Arrange
+            string emptyString = "";
+
+            // Act
+            string extension = emptyString.GetExtension();
+
+            // Assert
+            Assert.AreEqual(string.Empty, extension);
+        }
+
+        [Test]
+        public void NullString_GetExtension_ReturnsEmptyString()
+        {
+            // Arrange
+            string nullString = null;
+
+            // Act
+            string extension = nullString.GetExtension();
+
+            // Assert
+            Assert.AreEqual(string.Empty, extension);
+        }
+
+        [Test]
+        public void StringWithValidFileName_GetExtension_ReturnsExtension()
+        {
+            // Arrange
+            string filename = "test-filename.txt";
+
+            // Act
+            string extension = filename.GetExtension();
+
+            // Assert
+            Assert.AreEqual("txt", extension);
+        }
+
+        [Test]
+        public void StringWithInvalidFileName_GetExtension_ReturnsEmptyString()
+        {
+            // Arrange
+            string invalidFilename = "test-filename.";
+
+            // Act
+            string extension = invalidFilename.GetExtension();
+
+            // Assert
+            Assert.AreEqual(string.Empty, extension);
+        }
+    }
+}
