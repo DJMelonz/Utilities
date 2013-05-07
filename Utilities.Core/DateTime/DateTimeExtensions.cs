555ea4a3266d4cee0ad2f305a8c66bb1a174fb9d
DamianMullins <damian@lowflyingowls.co.uk>
2013-05-07 23:07:49 +0100

    Added DateTime DateTimeExtensions FirstDayOfWeek & tests

---
+++ Utilities.Core/DateTime/DateTimeExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace System.Utilities.Core.DateTime
+{
+    public static class DateTimeExtensions
+    {
+        public static System.DateTime FirstDayOfWeek(this System.DateTime input)
+        {
+            int delta = DayOfWeek.Monday - input.DayOfWeek;
+            System.DateTime monday = input.AddDays(delta);
+            return new System.DateTime(monday.Year, monday.Month, monday.Day);
+        }
+    }
+}
---
+++ Utilities.Tests/Utilities.Core/DateTime/DateTimeExtensionsTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Utilities.Core.DateTime;
+using NUnit.Framework;
+
+namespace System.Utilities.Tests.Utilities.Core.DateTime
+{
+    [TestFixture]
+    public class DateTimeExtensionsTests
+    {
+        [Test]
+        public void Date_FirstDayOfWeek_ReturnsDateTime()
+        {
+            // Arrange
+            System.DateTime date = new System.DateTime(2013, 1, 1);
+
+            // Act
+            System.DateTime result = date.FirstDayOfWeek();
+
+            // Assert
+            Assert.AreEqual(DayOfWeek.Monday, result.DayOfWeek);
+            Assert.AreEqual(new System.DateTime(2012, 12, 31), result);
+        }
+    }
+}
