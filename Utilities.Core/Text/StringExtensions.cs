a0c89a53438568d6224787d4f22914d9f39f31f5
DamianMullins <damian@lowflyingowls.co.uk>
2013-05-07 10:44:25 +0100

    Initial commit

---
+++ Utilities.Core/Text/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Utilities.Core.Text
+{
+    public static class StringExtensions
+    {
+        public static bool IsEmpty(this string input)
+        {
+            return string.IsNullOrEmpty(input);
+        }
+    }
+}
---
+++ Utilities.Tests/Utilities.Core/StringExtensionsTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Utilities.Core.Text;
+using NUnit.Framework;
+
+namespace System.Utilities.Tests.Utilities.Core
+{
+    [TestFixture]
+    public class StringExtensionsTests
+    {
+        [Test]
+        public void EmptyString_IsEmpty_ReturnsFalse()
+        {
+            // Arrange
+            string emptyString = "";
+
+            // Act
+            bool isEmpty = emptyString.IsEmpty();
+
+            // Assert
+            Assert.IsTrue(isEmpty, "String was empty");
+        }
+
+        [Test]
+        public void NonEmptyString_IsEmpty_ReturnsFalse()
+        {
+            // Arrange
+            string nonEmptyString = "Test String";
+
+            // Act
+            bool isNotEmpty = nonEmptyString.IsEmpty();
+
+            // Assert
+            Assert.IsFalse(isNotEmpty, "String was not empty");
+        }
+    }
+}
