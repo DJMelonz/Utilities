e5f611d1584bbb144aa39c11b0d3ad7d6a69cad6
DamianMullins <damian@lowflyingowls.co.uk>
2013-05-07 12:19:36 +0100

    Added number directory with int IsNullOrZero method & tests

---
+++ Utilities.Core/Numbers/IntExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace System.Utilities.Core.Numbers
+{
+    public static class IntExtensions
+    {
+        public static bool IsNullOrZero(this int? input)
+        {
+            return (!input.HasValue || input.Value == 0);
+        }
+    }
+}
---
+++ Utilities.Tests/Utilities.Core/Numbers/IntExtensionsTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Utilities.Core.Numbers;
+using NUnit.Framework;
+
+namespace System.Utilities.Tests.Utilities.Core.Numbers
+{
+    [TestFixture]
+    public class IntExtensionsTests
+    {
+        [Test]
+        public void ZeroInt_IsNullOrZero_ReturnsTrue()
+        {
+            // Arrange
+            int? nullInt = 0;
+
+            // Act
+            bool intIsZero = nullInt.IsNullOrZero();
+
+            // Assert
+            Assert.IsTrue(intIsZero);
+        }
+
+        [Test]
+        public void NullInt_IsNullOrZero_ReturnsTrue()
+        {
+            // Arrange
+            int? nullInt = null;
+
+            // Act
+            bool intIsNull = nullInt.IsNullOrZero();
+
+            // Assert
+            Assert.IsTrue(intIsNull);
+        }
+
+        [Test]
+        public void IntWithValue_IsNullOrZero_ReturnsTrue()
+        {
+            // Arrange
+            int? nonNullInt = 1;
+
+            // Act
+            bool intIsNotNullOrZero = nonNullInt.IsNullOrZero();
+
+            // Assert
+            Assert.IsFalse(intIsNotNullOrZero);
+        }
+    }
+}
