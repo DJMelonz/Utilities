d5df0b583cf65fea53a8fe5e683b6acd8107467a
DamianMullins <damian@lowflyingowls.co.uk>
2013-05-21 15:51:43 +0100

    Added numbers PercentageExtensions extensions

---
+++ Utilities.Core/Numbers/PercentageExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Utilities.Core.Numbers
+{
+    public static class PercentageExtensions
+    {
+        public static IEnumerable<double> Percentage(this IEnumerable<int> values)
+        {
+            var list = values.ToList();
+            int sum = list.Sum();
+
+            return list.Select(value => (double)value / (double)sum);
+        }
+
+        public static IEnumerable<double> Percentage(this IEnumerable<long> values)
+        {
+            var list = values.ToList();
+            long sum = list.Sum();
+
+            return list.Select(value => (double)value / (double)sum);
+        }
+
+        public static IEnumerable<double> Percentage(this IEnumerable<double> values)
+        {
+            var list = values.ToList();
+            double sum = list.Sum();
+
+            return list.Select(value => value / sum);
+        }
+    }
+}
