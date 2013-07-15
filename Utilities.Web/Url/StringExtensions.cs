cb23a6722e809e72137ead105a00d1268a9ec186
Damian <damian@lowflyingowls.co.uk>
2013-07-15 13:18:33 +0100

    Added url extensions. Enabled package restore. Added moq

--- Utilities.Web/Url/StringExtensions.cs
+++ Utilities.Web/Url/StringExtensions.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
+using Utilities.Core.Text;
 
 namespace Utilities.Web.Url
 {
-    class StringExtensions
+    public static class StringExtensions
     {
+        public static string ToSlug(this string input, int? maxLength = null)
+        {
+            if (input.IsNotEmpty())
+            {
+                input = input.ToLower();
+                // Remove entities
+                input = Regex.Replace(input, @"&\w+;", "");
+                // Remove anything that is not letters, numbers, dash, or space
+                input = Regex.Replace(input, @"[^a-z0-9\-\s]", "");
+                // Replace spaces
+                input = input.Replace(' ', '-');
+                // Collapse dashes
+                input = Regex.Replace(input, @"-{2,}", "-");
+                // Trim excessive dashes at the beginning
+                input = input.TrimStart(new[] { '-' });
+                // Truncate if a value has been provided
+                if (maxLength.HasValue)
+                {
+                    // if it's too long, clip it
+                    if (input.Length > maxLength.Value - 1)
+                    {
+                        input = input.Substring(0, maxLength.Value);
+                    }
+                }
+                // Remove trailing dashes
+                input = input.TrimEnd(new[] { '-' });
+
+                return input;
+            }
+            return string.Empty;
+        }
     }
 }
